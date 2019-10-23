e315e3114c3fd3c0c0ddc842bb08c5739f12764f
MuzammalHussain6313 <muzammal.bsse3310@iiu.edu.pk>
2019-10-23 12:22:04 +0500

    node REST API initial commit for version v1.0.0

---
+++ index.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/studentDB', {useNewUrlParser: true});
+
+const Student = mongoose.model('Student', {
+    name: String,
+    student_id: Number,
+    email: String,
+    password: String,
+    date_added: Date
+});
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+// app.post('/login',  async (req, res) => {
+//     const body = req.body;
+//     console.log('req.body', body);
+//     const email = body.email;
+//     // lets check if email exists
+//     const result = await Student.findOne({"email":  email});
+//
+//     if(!result) // this means result is null
+//     {
+//         res.status(401).send({
+//             Error: 'This user doesnot exists. Please signup first'
+//         });
+//     }
+//     else{
+//         // email did exist
+//         // so lets match password
+//         if(body.password === result.password){
+//             // great, allow this user access
+//             console.log('match');
+//             res.send({message: 'Successfully Logged in'});
+//         }
+//         else{
+//             console.log('password doesnot match');
+//             res.status(401).send({message: 'Wrong email or Password'});
+//         }
+//     }
+//     console.log('result', result);
+//     // 2. if exists, check if password matches
+//     res.send({ result: result });
+//
+// });
+
+app.post('/login',  async (req, res) => {
+    const body = req.body;
+    console.log('req.body', body);
+
+    const email = body.email;
+
+    // lets check if email exists
+
+    const result = await Student.findOne({"email":  email});
+    if(!result) // this means result is null
+    {
+        res.status(401).send({
+            Error: 'This user doesnot exists. Please signup first'
+        });
+    }
+    else{
+        // email did exist
+        // so lets match password
+
+        if(body.password === result.password){
+
+            // great, allow this user access
+
+            console.log('match');
+
+            res.send({message: 'Successfully Logged in'});
+        }
+
+        else{
+
+            console.log('password doesnot match');
+
+            res.status(401).send({message: 'Wrong email or Password'});
+        }
+
+
+    }
+
+});
+app.post('/signup', async (req, res) => {
+    const body = req.body;
+    console.log('req.body', body)
+    try{
+        const student = new Student(body);
+        const result = await student.save();
+        res.send({
+            message: 'Student signup successful'
+        });
+    }
+    catch(ex){
+        console.log('ex',ex);
+        res.send({message: 'Error'}).status(401);
+    }
+});
+app.get('/getStudents', async (req, res)=>{
+    const allStudents = await Student.find();
+    console.log('allStudents', allStudents);
+
+    res.send(allStudents);
+})
+
+app.listen(3000, () => {
+    console.log('Express application running on localhost:3000');
+});
